64a8da03960a98b5476044ef74a849c389f7f00a
karlosjota <cj.gomlop@gmail.com>
2015-07-23 19:06:12 +0200

    Primera pregunta

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,15 @@
+// primera pregunta
+
+//GET /quizes/question
+exports.question = function(req, res) {
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+};
+
+//GET /quizes/answer
+exports.answer = function(req, res) {
+	if (req.query.respuesta === 'Roma') {
+		res.resnder('quizes/answer', {respuesta: 'Correcto'});
+	} else {
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	}
+};
